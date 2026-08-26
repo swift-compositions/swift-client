8736e11726ecd1da2baffbcdcccd622059ab4d2c
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-26 06:30:18 +0200

    Make Client the generic operation arrow

--- Sources/Client/Client.swift
+++ Sources/Client/Client.swift
@@ -1,6 +1,6 @@
 public struct Client<Input, Output, Failure: Swift.Error> {
 
-    public var run: (Input) async throws(Failure) -> Output
+    public let run: (Input) async throws(Failure) -> Output
 
     public init(
         run: @escaping (Input) async throws(Failure) -> Output
